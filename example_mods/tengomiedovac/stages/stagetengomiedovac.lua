34cfffbeff2245deccecaf34c0612ecfa57c10e5
PbtinaDev <pbt@pbtservers.com>
2025-01-24 01:44:23 +0100

    Arreglar TengoMiedoVAC

--- example_mods/tengomiedovac/stages/stagetengomiedovac.lua
+++ example_mods/tengomiedovac/stages/stagetengomiedovac.lua
@@ -1,7 +1,7 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('bgproblemvac', 'bgproblemvac', -600, -300);
-	setScrollFactor('bgproblemvac', 0.9, 0.9);
+	makeLuaSprite('bgtengomiedovac', 'bgtengomiedovac', -600, -300);
+	setScrollFactor('bgtengomiedovac', 0.9, 0.9);
 
-	addLuaSprite('bgproblemvac', false);
+	addLuaSprite('bgtengomiedovac', false);
 end
--- example_mods/tengomiedovac/stages/stagetoxicidad.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('bgtoxicidad', 'bgtoxicidad', -600, -300);
-	setScrollFactor('bgtoxicidad', 0.9, 0.9);
-
-	addLuaSprite('bgtoxicidad', false);
-end
